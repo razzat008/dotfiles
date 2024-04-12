9076b51fd259eccfce012bc5c0ecc278a8a99f06
Razzat Dahal <rajat4623@student.ku.edu.np>
2024-04-12 10:03:54 +0545

    updated: config files for dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -12,8 +12,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char dmenufont[] = "Iosevka:size=10";
-static const char *fonts[] = {"Iosevka:style:medium:size=10", "JetBrainsMono Nerd Font:style:medium:size=10", "Material Design Icons Desktop:size=9"};
+static const char dmenufont[] = "Hack Nerd Font:size=10";
+static const char *fonts[] = {"Hack Nerd Font:style:medium:size=10", "Hack Nerd Font:style:medium:size=10", "Symbols Nerd Font:size=9"};
 static const char col_gray1[] = "#222222"; // backgroud colour normally seen
 static const char col_gray2[] = "#E8B082"; // border for unfocused window
 static const char col_gray3[] = "#fff";    // font color ;normal
@@ -32,7 +32,7 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"", "", "" ,"", "5", "", " ", "8", "9"};
+static const char *tags[] = {"", "", "" ,"", "5", "󰙯", "7", "8", "9"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -41,15 +41,14 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
    {"LibreWolf",    NULL,       NULL,       1 <<1 ,       0,           -1},
-   {"Brave-browser",    NULL,       NULL,       1 <<1 ,       0,           -1},
    {"Signal",       NULL,       NULL,       1 <<3  ,      0,           -1 },
+   {"TelegramDesktop", NULL,    NULL,       1 << 3,       0,           -1 },
+   {"jetbrains-pycharm-ce", NULL,    NULL,       1 << 2,       0,           -1 },
+
    {"discord",      NULL,       NULL,       1 <<5  ,      0,           -1 },
-   {"thunderbird",  NULL,       NULL,       1 <<6  ,      0,           -1 },
-   {"Nitrogen",        NULL,       NULL,       0,                1,        -1},
-   {"Pavucontrol",        NULL,       NULL,       0,                1,        -1},
-   {"Gscreenshot",     NULL,       NULL,       0,                1,        -1},
-   {"TelegramDesktop", NULL,       NULL,       1 << 3,           0,        -1},
-   {"Element", NULL,       NULL,       1 << 3,           0,        -1},
+   // {"thunderbird",  NULL,       NULL,       1 <<6  ,      0,           -1 },
+   {"Nitrogen",     NULL,       NULL,       0,            1,           -1 },
+   {"Pavucontrol",  NULL,       NULL,       0,            1,           -1 },
 
 };
 
@@ -75,6 +74,7 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define ALTKEY Mod1Mask
 #define printsc 0xff61
+#define f9key 0xffc6
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -88,30 +88,30 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb",
 col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
   // custom keybindings start {start}
-  {0, XF86XK_AudioRaiseVolume,  spawn, SHCMD("~/.scripts/volumenotifier up")},
-  {0, XF86XK_AudioLowerVolume,  spawn, SHCMD("~/.scripts/volumenotifier down")},
-  {0, XF86XK_AudioMute,         spawn, SHCMD("~/.scripts/volumenotifier mute")},
+  {0, XF86XK_AudioRaiseVolume,  spawn, SHCMD("~/.local/bin/volumenotifier up")},
+  {0, XF86XK_AudioLowerVolume,  spawn, SHCMD("~/.local/bin/volumenotifier down")},
+  {0, XF86XK_AudioMute,         spawn, SHCMD("~/.local/bin/volumenotifier mute")},
   {0, XF86XK_RFKill,            spawn, SHCMD("rfkill toggle all")},
-  {0, XF86XK_AudioPlay,         spawn, SHCMD("mpc toggle")},
-  {0, XF86XK_AudioPrev,         spawn, SHCMD("mpc prev")},
-  {0, XF86XK_AudioNext,         spawn, SHCMD("mpc next")},
-  {0, printsc,                  spawn, SHCMD("gscreenshot")}, //to take screenshot
-  {MODKEY, printsc,                  spawn, SHCMD("flameshot gui")}, //to take screenshot
-  {MODKEY | ShiftMask, XK_p, spawn, SHCMD("~/.scripts/power_menu")},
-  {ALTKEY, 0xff52, spawn, SHCMD("~/.scripts/bright_notif up")},
-  {ALTKEY, 0xff54, spawn, SHCMD("~/.scripts/bright_notif down")},
-  {MODKEY, XK_n,   spawn, SHCMD("nemo")},
+  {0, XF86XK_AudioPlay,         spawn, SHCMD("~/.local/bin/MediaControl --toggle")},
+  {0, XF86XK_AudioPrev,         spawn, SHCMD("~/.local/bin/MediaControl --previous")},
+  {0, XF86XK_AudioNext,         spawn, SHCMD("~/.local/bin/MediaControl --next")},
+  {0, f9key,                    spawn, SHCMD("betterlockscreen -l blur")}, //to lock screen
+  {0, printsc,                  spawn, SHCMD("flameshot gui")}, //to take screenshot
+  {MODKEY | ShiftMask, XK_p, spawn, SHCMD("~/.local/bin/power_menu")},
+  {MODKEY | ShiftMask, XK_x, spawn, SHCMD("~/.local/bin/killallinstances")},
+  {MODKEY | ShiftMask, XK_w, spawn, SHCMD("~/.local/bin/rofiwificonnection")},
+  {ALTKEY, 0xff52, spawn, SHCMD("~/.local/bin/bright_notif up")},
+  {ALTKEY, 0xff54, spawn, SHCMD("~/.local/bin/bright_notif down")},
+  {MODKEY, XK_n,   spawn, SHCMD("thunar")},
   {ALTKEY, XK_f,   spawn, SHCMD("librewolf")},
   {ALTKEY, XK_t,   spawn, SHCMD("telegram-desktop")},
   {ALTKEY, XK_b,   spawn, SHCMD("firefox")},
-  {ALTKEY, XK_m,   spawn, SHCMD("st ncmpcpp")},
-  {ALTKEY, XK_s,   spawn, SHCMD("st ranger ~/school")},
+  {ALTKEY, XK_z,   spawn, SHCMD("st ranger ~/school")},
   {ALTKEY, XK_p,   spawn, SHCMD("librewolf --private-window --new-tab")},
-  {ALTKEY, XK_n,   spawn, SHCMD("brave")}, // to Launch brave browser
   {MODKEY, XK_d, spawn, SHCMD("rofi -show drun")},
   {MODKEY, XK_c, spawn, SHCMD("rofi -show calc -modi calc -no-show-match -no-sort")},
   {MODKEY, XK_e, spawn, SHCMD("rofi -show emoji")},
